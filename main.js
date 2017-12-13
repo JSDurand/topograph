a924fb8303e417f2ab7ccbc8e3cf5139b3b72427
Durand <mmemmew@gmail.com>
2017-12-13 10:00:16 +0800

    an attempt at calculating the topographs for forms whose associated roots are of the same sign, but still with flaws

--- main.js
+++ main.js
@@ -67,12 +67,152 @@ function Submitbutton () {
   else {topograph(parseInt(a),parseInt(b),parseInt(c));}
 }
 
+// calculate the positive direction
+function routine_positive_calculations (init_1, init_2, init_3) {
+  var repeated = false; // A variable to determine if we shall stop.
+  var graph    = {};
+
+  for (var area_array = [init_1, init_2, init_3]; !repeated;) {
+    graph.positive_indices = [];
+    graph.negative_indices = [];
+
+    var l = area_array.length;
+
+    console.log(area_array);
+
+    for (var i = 0; i < l; i++) {
+      if (area_array[i].valeur > 0) {graph.positive_indices.push(i);}
+      else if (area_array[i].valeur < 0) {graph.negative_indices.push(i);}
+      else {alert('Don\'t input a form which represents 0!');return;}
+    }
+
+    var last_pos = area_array[graph.positive_indices.last()];
+    var last_neg = area_array[graph.negative_indices.last()];
+
+    if (area_array.last().valeur < 0) {
+      var ntl_neg   = area_array[graph.negative_indices.next_to_last()];
+      var new_vec_x = area_array.last().x + last_pos.x;
+      var new_vec_y = area_array.last().y + last_pos.y;
+      var new_val   = 2 * (last_neg.valeur + last_pos.valeur) - ntl_neg.valeur;
+      if (new_val === 0) {alert('Don\'t input a form which represents 0!!!');return area_array;}
+      // alert(ntl_neg);
+      area_array.push(create_area(new_vec_x, new_vec_y, new_val));
+    } else if (area_array.last().valeur > 0) {
+      var ntl_pos   = area_array[graph.positive_indices.next_to_last()];
+      var new_vec_x = area_array.last().x + last_neg.x;
+      var new_vec_y = area_array.last().y + last_neg.y;
+      var new_val   = 2 * (last_neg.valeur + last_pos.valeur) - ntl_pos.valeur;
+      if (new_val === 0) {alert('Don\'t input a form which represents 0!!!');return area_array;}
+      area_array.push(create_area(new_vec_x, new_vec_y, new_val));
+    }
+    repeated = init_2.valeur === last_pos.valeur && init_1.valeur === last_neg.valeur && init_3.valeur === new_val;
+  }
+
+  return area_array;
+}
+
+// calculate the negative direction
+function routine_negative_calculations (init_1, init_2, init_3) {
+  var repeated = false; // reset the variable
+  var graph    = {};
+
+  for (var neg_area_array = [init_1, init_2, init_3]; !repeated;) {
+    graph.positive_indices = [];
+    graph.negative_indices = [];
+
+    var l = neg_area_array.length;
+
+    for (var i = 0; i < l; i++) {
+      if (neg_area_array[i].valeur > 0) {graph.positive_indices.push(i);}
+      else if (neg_area_array[i].valeur < 0) {graph.negative_indices.push(i);}
+      else {alert('Don\'t input a form which represents 0!');return;}
+    }
+
+    var last_pos = neg_area_array[graph.positive_indices.last()];
+    var last_neg = neg_area_array[graph.negative_indices.last()];
+
+    if (neg_area_array.last().valeur < 0) {
+      var ntl_neg   = neg_area_array[graph.negative_indices.next_to_last()];
+      var new_val   = 2 * (last_neg.valeur + last_pos.valeur) - ntl_neg.valeur;
+      if (new_val > 0) {
+        var new_vec_x = last_pos.x - last_neg.x;
+        var new_vec_y = last_pos.y - last_neg.y;
+      } else if (new_val < 0) {
+        var new_vec_x = last_neg.x - last_pos.x;
+        var new_vec_y = last_neg.y - last_pos.y;
+      } else {
+        alert('Don\'t input a form which represents 0!!!');
+        return neg_area_array;
+      }
+      neg_area_array.push(create_area(new_vec_x, new_vec_y, new_val));
+    } else if (neg_area_array.last().valeur > 0) {
+      var ntl_pos   = neg_area_array[graph.positive_indices.next_to_last()];
+      var new_val   = 2 * (last_neg.valeur + last_pos.valeur) - ntl_pos.valeur;
+      if (new_val > 0) {
+        var new_vec_x = last_pos.x - last_neg.x;
+        var new_vec_y = last_pos.y - last_neg.y;
+      } else if (new_val < 0) {
+        var new_vec_x = last_neg.x - last_pos.x;
+        var new_vec_y = last_neg.y - last_pos.y;
+      } else {
+        alert('Don\'t input a form which represents 0!!!');
+        return neg_area_array;
+      }
+      neg_area_array.push(create_area(new_vec_x, new_vec_y, new_val));
+    }
+    repeated = init_2.valeur === last_pos.valeur && init_1.valeur === last_neg.valeur && init_3.valeur === new_val;
+  }
+  return neg_area_array;
+}
+
+function gcd (n, m) {
+  var r = 0;
+  while (n !== 0) {
+    r = m % n;
+    m = n;
+    n = r;
+  }
+  return m;
+};
+
+function find_initial_vector (a, b, c) {
+  var initial_vector = {
+    x : 0,
+    y : 1,
+  };
+
+  function weird_form (x) {
+    return x * x + b * x * y + a * c * y * y;
+  }
+
+  var found      = false;
+  var value      = a * c;
+  var value_prec = 1 + Math.abs(b) + a * c;
+
+  for (var i = 1; 2 * i < Math.abs(b); i++) {
+    temp       = value;
+    value      = 2 * (value + 1) - value_prec;
+    value_prec = temp;
+    if (value < 0) {
+      found = true;
+      var g = gcd(i,a);
+      var x = (b < 0) ? i / g : -i / g;
+      var y = a / g;
+      initial_vector.x = x;
+      initial_vector.y = y;
+      initial_vector.valeur = a * x * x + b * x * y + c * y * y;
+
+      break;
+    }
+  }
+
+  if (found) {return initial_vector;} else {alert('not found!');return;}
+  
+}
+
 function topograph (a,b,c) {
   // calculate the topograph of the form ax^2+bxy+cy^2 and draw on the 
   // context context
-
-  //can     = document.getElementById('can');
-  //context = can.getContext('2d');
 
   // We first deal with the case ac > 0.
   // We suppose without loss of generality that a > 0.
@@ -85,106 +225,52 @@ function topograph (a,b,c) {
   
   if (a < 0) {topograph(-a,-b,-c);return;}
   if (b*b<=4*a*c) {alert('this is positive definite!');return;}
-  if (a*c < 0) {
+  if (c < 0) { // a * c < 0 if and only if c < 0
     // a relatively easy case
     // (1,0) -> a
     // (0,1) -> c
 
-    // var form = function (x, y) {
-      // return a * x * x + b * x * y + c * y * y;
-    // }
-
     var init0 = create_area(0,1,c);      // p_{-1}
     var init1 = create_area(1,0,a);      // p_0
     var init2 = create_area(1,1,a+b+c);  // p_0+p_{-1}
-    var init3 = (a-b+c < 0) ? create_area(-1,1,a-b+c) : create_area(1,-1,a-b+c); // negative direction
+    var init3 = (a-b+c < 0) ? create_area(-1,1,a-b+c) : create_area(1,-1,a-b+c); // for negative direction
 
-    var repeated = false; // A variable to determine if we shall stop.
+    var area_array     = routine_positive_calculations(init0, init1, init2);
+    var neg_area_array = routine_negative_calculations(init0, init1, init3);
+  } else if (c > 0) {
+    // Now deal with the complex case
+    // We need first to find the initial vectors of the river,
+    // and the rest is nothing more than routine calculations.
+    
+    // To find the initial vector, we first multiply the form by a
+    // to suppose that a = 1. In this case we try integers after integers
+    // to find a critical integer, and then divide a back.
+    // We don't need to actually try out every integer, as we have an
+    // upper bound by b/2.
+    // alert('Only the case a*c < 0 and a > 0 is implemented!');
 
-    for (var area_array = [init0, init1, init2]; !repeated;) {
-      graph.positive_indices = [];
-      graph.negative_indices = [];
-
-      var l = area_array.length;
-
-      for (var i = 0; i < l; i++) {
-        if (area_array[i].valeur > 0) {graph.positive_indices.push(i);}
-        else if (area_array[i].valeur < 0) {graph.negative_indices.push(i);}
-      }
-
-      var last_pos = area_array[graph.positive_indices.last()];
-      var last_neg = area_array[graph.negative_indices.last()];
-
-      if (area_array.last().valeur < 0) {
-        var ntl_neg   = area_array[graph.negative_indices.next_to_last()];
-        var new_vec_x = area_array.last().x + last_pos.x;
-        var new_vec_y = area_array.last().y + last_pos.y;
-        var new_val   = 2 * (last_neg.valeur + last_pos.valeur) - ntl_neg.valeur;
-        if (new_val === 0) {alert('Don\'t input a form which represents 0!!!');return area_array;}
-        // alert(ntl_neg);
-        area_array.push(create_area(new_vec_x, new_vec_y, new_val));
-      } else if (area_array.last().valeur > 0) {
-        var ntl_pos   = area_array[graph.positive_indices.next_to_last()];
-        var new_vec_x = area_array.last().x + last_neg.x;
-        var new_vec_y = area_array.last().y + last_neg.y;
-        var new_val   = 2 * (last_neg.valeur + last_pos.valeur) - ntl_pos.valeur;
-        if (new_val === 0) {alert('Don\'t input a form which represents 0!!!');return area_array;}
-        area_array.push(create_area(new_vec_x, new_vec_y, new_val));
-      }
-      repeated = init1.valeur === last_pos.valeur && init0.valeur === last_neg.valeur && init2.valeur === new_val;
+    function form (a, b, c, x, y) {
+      // this will be used only several times.
+      return a * x * x + b * x * y + c * y * y;
     }
 
-    var repeated = false; // reset the variable
+    var initial_vector = find_initial_vector(a,b,c);
 
-    for (var neg_area_array = [init0, init1, init3]; !repeated;) {
-      // calculate the negative direction
-      graph.positive_indices = [];
-      graph.negative_indices = [];
+    var value0 = initial_vector.valeur;
+    var value1 = form(a,b,c,initial_vector.x,initial_vector.y); // < 0
+    var value2 = form(a,b,c,initial_vector.x+1,initial_vector.y);
+    var value3 = form(a,b,c,initial_vector.x-1,initial_vector.y);
 
-      var l = neg_area_array.length;
+    var init0 = create_area(initial_vector.x, initial_vector.y, value1);
+    var init1 = create_area(1,0,a);
+    var init2 = create_area(initial_vector.x+1, initial_vector.y, value2);
+    var init3 = (value3 < 0) ? create_area(initial_vector.x-1,initial_vector.y,value3) : create_area(1-initial_vector.x,-initial_vector.y,value3);
 
-      for (var i = 0; i < l; i++) {
-        if (neg_area_array[i].valeur > 0) {graph.positive_indices.push(i);}
-        else if (neg_area_array[i].valeur < 0) {graph.negative_indices.push(i);}
-      }
-
-      var last_pos = neg_area_array[graph.positive_indices.last()];
-      var last_neg = neg_area_array[graph.negative_indices.last()];
-
-      if (neg_area_array.last().valeur < 0) {
-        var ntl_neg   = neg_area_array[graph.negative_indices.next_to_last()];
-        var new_val   = 2 * (last_neg.valeur + last_pos.valeur) - ntl_neg.valeur;
-        if (new_val > 0) {
-          var new_vec_x = last_pos.x - last_neg.x;
-          var new_vec_y = last_pos.y - last_neg.y;
-        } else if (new_val < 0) {
-          var new_vec_x = last_neg.x - last_pos.x;
-          var new_vec_y = last_neg.y - last_pos.y;
-        } else {
-          alert('Don\'t input a form which represents 0!!!');
-          return neg_area_array;
-        }
-        neg_area_array.push(create_area(new_vec_x, new_vec_y, new_val));
-      } else if (neg_area_array.last().valeur > 0) {
-        var ntl_pos   = neg_area_array[graph.positive_indices.next_to_last()];
-        var new_val   = 2 * (last_neg.valeur + last_pos.valeur) - ntl_pos.valeur;
-        if (new_val > 0) {
-          var new_vec_x = last_pos.x - last_neg.x;
-          var new_vec_y = last_pos.y - last_neg.y;
-        } else if (new_val < 0) {
-          var new_vec_x = last_neg.x - last_pos.x;
-          var new_vec_y = last_neg.y - last_pos.y;
-        } else {
-          alert('Don\'t input a form which represents 0!!!');
-          return neg_area_array;
-        }
-        neg_area_array.push(create_area(new_vec_x, new_vec_y, new_val));
-      }
-      repeated = init1.valeur === last_pos.valeur && init0.valeur === last_neg.valeur && init3.valeur === new_val;
-    }
-
+    var area_array     = routine_positive_calculations(init0,init1,init2);
+    var neg_area_array = routine_negative_calculations(init0,init1,init3);
   } else {
-    alert('Only the case a*c < 0 and a > 0 is implemented!');
+    alert('Don\'t input a form which represents 0!');
+    return;
   }
 
   draw_area_array(area_array);
